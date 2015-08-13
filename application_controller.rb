0aa2415977484cc4bb1dfcf7478a09735b783047
HarukaSenju <hasenju@gmail.com>
2015-08-13 18:56:09 +0000

    final

--- application_controller.rb
+++ application_controller.rb
@@ -1,3 +1,4 @@
+require_relative 'models/model.rb'
 require 'bundler'
 Bundler.require
 
@@ -5,6 +6,18 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  
+  get '/trump' do
+    erb :trump
+  end
+  post '/trump' do
+    puts params
+    
+    @quote = Quote.new(params[:topic])
+    puts @quote.quote
+    #@trump1 = Trump.new(params[:topic])
+    erb :trump
   end
 
 end
