b4ce1014b54597038f52ecf06c8672bdedfcde9c
HarukaSenju <hasenju@gmail.com>
2015-08-14 17:20:03 +0000

    finished ccs

--- application_controller.rb
+++ application_controller.rb
@@ -13,11 +13,15 @@ class ApplicationController < Sinatra::Base
   end
   post '/trump' do
     puts params
-    
+    @pictures = ["http://i.imgur.com/agswDP4.gif","http://i.imgur.com/ItwbdfI.gif","http://cdn.playbuzz.com/cdn/885e818d-ca76-4ad4-81c1-62cdba619482/05b87b73-0ead-41f3-83dd-4bc060755120.gif"]
     @quote = Quote.new(params[:topic])
     puts @quote.quote
     #@trump1 = Trump.new(params[:topic])
     erb :trump
+  end
+  
+  get '/index.erb' do
+    erb :index
   end
 
 end
